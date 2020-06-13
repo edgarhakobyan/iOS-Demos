736d7e3c017eff3a51b6d3482aefbc270b5b2f41
Edgar Hakobyan <ehakobyan94@gmail.com>
2020-06-13 17:53:51 +0400

    Bitcoin app - MVC, HTTPS, Delegate, Picker View

---
+++ BitCoin/BitCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  BitCoin
+//
+//  Created by Edgar on 6/11/20.
+//  Copyright © 2020 Edgar. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
