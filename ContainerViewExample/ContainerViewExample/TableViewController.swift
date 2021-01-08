d09801784353dcc73e38e225afc2a04747c30024
Edgar Hakobyan <ehakobyan94@gmail.com>
2021-01-09 01:08:52 +0400

    ContainerViewExample

---
+++ ContainerViewExample/ContainerViewExample/TableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  TableViewController.swift
+//  ContainerViewExample
+//
+//  Created by Edgar on 09.01.21.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let imageNameArray = ["Alberto Ruiz - 7 Elements (Original Mix)",
+                          "Dave Wincent - Red Eye (Original Mix)",
+                          "E-Spectro - End Station (Original Mix)",
+                          "Edna Ann - Phasma (Konstantin Yoodza Remix)",
+                          "Ilija Djokovic - Delusion (Original Mix)",
+                          "John Baptiste - Mycelium (Original Mix)",
+                          "Lane 8 - Fingerprint (Original Mix)",
+                          "Mac Vaughn - Pink Is My Favorite Color (Alex Stein Remix)",
+                          "Metodi Hristov, Gallya - Badmash (Original Mix)",
+                          "Veerus, Maxie Devine - Nightmare (Original Mix)"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageNameArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TitleCell", for: indexPath)
+        cell.imageView?.image = UIImage(named: imageNameArray[indexPath.row])
+        cell.textLabel?.text = imageNameArray[indexPath.row]
+        cell.textLabel?.numberOfLines = 2
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let parentVC = parent as? ViewController {
+            let trackTitle = imageNameArray[indexPath.row]
+            parentVC.textLabel.text = trackTitle
+            parentVC.imageView.image = UIImage(named: trackTitle)
+        }
+    }
+    
+}
---
+++ ContainerViewExample/ContainerViewExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ContainerViewExample
+//
+//  Created by Edgar on 09.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
